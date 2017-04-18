0221b49e5999186562c951a81f323ef84955b945
Tsai-Tse Tu <JudyTu@D-69-91-181-111.dhcp4.washington.edu>
2017-04-18 12:20:12 -0700

    modify rsa

--- rsa.py
+++ rsa.py
@@ -42,15 +42,27 @@ def encryption(m):
     """
     This function creates encrypted message (ciphertext)
     """
-    encryptM = [(ord(c) ** e % n) for c in m]
+    #asciiM = [ord(c) for c in m]
+    asciiM2bits = [bin(ord(x))[2:].zfill(8) for x in m]
+    concatenateBits = ''.join(asciiM2bits)
+    decimalM = int(concatenateBits, 2)
+    encryptM = (decimalM**e)%n
+    #print type(encryptM)
     return encryptM
 
 def decryption(c):
     """
     This function decrypts the ciphertext to the original message 
     """
-    decryptM = [chr(char ** d % n) for char in c]
-    return ''.join(decryptM)
+    decimalC = (c**d)%n
+    print decimalC
+    bits_C = format(decimalC, 'b')
+    print bits_C
+    bytes_C = [bits_C[i:i+2] for i in range(0, len(bits_C), 2)]
+    print bytes_C
+    #decryptM = [chr(char ** d % n) for char in c]
+    
+    #return ''.join(decryptM)
 
 p = 23 # p can be changed
 q = 29 # q can be changed
@@ -58,12 +70,19 @@ n = calcN(p,q)
 phi = calcPhi(p,q)
 e = createPubKeys(phi)
 d = createPrivKeys(e, phi)
+#print type(d)
 
 message = raw_input("Please enter your desired message to be encrypted: ")
-
+e = encryption(message)
+decryption(e)
+#decryption(encryption(message))
 #assert decryption(encryption(message)) == message
 
-print "Decrypted Message is:", decryption(encryption(message))
-     
-     
-       
+#print "Decrypted Message is:", decryption(encryption(message))
+
+
+#encryption('hi how are you')
+
+
+
+
